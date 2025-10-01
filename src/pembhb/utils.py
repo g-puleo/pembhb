53e2a0d10163feb320f262888392fb010a4ca144
gianmarco-coglians <74814411+g-puleo@users.noreply.github.com>
2025-10-01 14:06:03 +0200

    update pyhton version

--- src/pembhb/utils.py
+++ src/pembhb/utils.py
@@ -100,9 +100,8 @@ def get_logratios_grid(dataset: MBHBDataset, model: 'InferenceNetwork', ngrid_po
         results = torch.cat(results, dim=0).numpy()
         injection_params = torch.cat(injection_params, dim=0).numpy()
         grid = grid.detach().cpu().numpy()
-    return results, injection_params, grid
 
-def get_logratios_grid_2d(dataset: MBHBDataset, model: InferenceNetwork, ngrid_points: int, out_param_idx : int, in_param_idx : tuple):
+def get_logratios_grid_2d(dataset: MBHBDataset, model: 'InferenceNetwork', ngrid_points: int, out_param_idx : int, in_param_idx : tuple):
     dataloader = DataLoader(dataset, batch_size=min(10, len(dataset)), shuffle=False)
     results = []
     injection_params = []
@@ -240,7 +239,7 @@ def get_pvalues_2d(logratios: np.array, grid_0: np.array , grid_1: np.array, inj
 
 
 
-def update_bounds(model: InferenceNetwork, observation_dataset: MBHBDataset, priordict: dict, in_param_idx: int, n_gridpoints: int = 100000, out_param_idx: int = None, eps: float = 1e-5):
+def update_bounds(model: 'InferenceNetwork', observation_dataset: MBHBDataset, priordict: dict, in_param_idx: int, n_gridpoints: int = 100000, out_param_idx: int = None, eps: float = 1e-5):
     """Update the prior bounds based on the posterior obtained from a model on a single observation. 
     Used to do truncation in MNRE. 
 
@@ -322,16 +321,6 @@ def pp_plot_2d(dataset, model,  in_param_idx: tuple, out_idx: int, name: str):
     if name is not None:
         fig.savefig(os.path.join(ROOT_DIR, "plots", f"{name}_pp_plot_2d.png"))
     plt.close()
-if __name__ == "__main__":
-    fname = "/u/g/gpuleo/pembhb/logs_0729/peregrine_norm/version_1/checkpoints/epoch=93-step=16920.ckpt"
-    conf = read_config(os.path.join(ROOT_DIR,"config.yaml"))
-    model = InferenceNetwork.load_from_checkpoint(fname, conf=conf)
-    model.eval()
-
-    dataset = MBHBDataset(os.path.join(ROOT_DIR, "data/observation.h5"))
-
-    updated_prior = update_bounds(model, dataset, conf, parameter_idx=1, n_gridpoints=100)
-    print(updated_prior)
 
 
 def chirp_mass_from_m1m2(m1, m2):
